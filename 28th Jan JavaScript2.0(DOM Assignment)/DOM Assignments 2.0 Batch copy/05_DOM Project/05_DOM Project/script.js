7c5c9452da033a9f693b20941dcf02968c7d33c8
Akshay-1201 <akshaykute12@gmail.com>
2023-05-08 16:45:44 +0530

    commit

--- 28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/05_DOM Project/05_DOM Project/script.js
+++ 28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/05_DOM Project/05_DOM Project/script.js
@@ -8,21 +8,21 @@ navb.appendChild(but);
 
 // adding tag name
 
-// let tagname = document.querySelector(".tags-container div");
-// let link = document.createElement("a");
-// link.href = "#";
-// link.innerText = "Chinese (7)" ;
-// tagname.appendChild(link);
+let tagname = document.querySelector(".tags-container div");
+let link = document.createElement("a");
+link.href = "#";
+link.innerText = "Chinese (7)" ;
+tagname.appendChild(link);
 
 //  create card
-//  const gallery = document.querySelector(".recipe-gallery");
+const gallery = document.querySelector(".recipe-gallery");
 
-//  let car = document.createElement("div");
-//  car.className = "card";
-//  gallery.appendChild(car);
-// let par = document.createElement("h5");
-// par.innerText = "add 6th card here";
-// car.appendChild(par);
+ let car = document.createElement("div");
+ car.className = "card";
+ gallery.appendChild(car);
+let par = document.createElement("h5");
+par.innerText = "add 6th card here";
+car.appendChild(par);
 
 // change name 
 let names = document.getElementsByTagName("a");
--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@
 # [Dom 3](https://github.com/Akshay-1201/DOM_assignments/tree/main/28th%20Jan%20JavaScript2.0(DOM%20Assignment)/DOM%20Assignments%202.0%20Batch%20copy/DOM%20Assignment%202.0%201%2C2%2C3/thirdAssignmentImage)
 
 # [Dom 4](https://github.com/Akshay-1201/DOM_assignments/blob/main/28th%20Jan%20JavaScript2.0(DOM%20Assignment)/DOM%20Assignments%202.0%20Batch%20copy/04_DOM%20Project/readme.md)
+
+# [Dom 5](https://github.com/Akshay-1201/DOM_assignments/blob/main/28th%20Jan%20JavaScript2.0(DOM%20Assignment)/DOM%20Assignments%202.0%20Batch%20copy/05_DOM%20Project/readme.md)
